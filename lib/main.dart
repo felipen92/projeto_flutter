eb702ba1c05fe0960fe4a5b780e8154c61df7d0a
Felipe Barros <felipe92barros@gmail.com>
2020-04-27 21:51:56 -0300

    Adicionando implementação da calculadora de IMC

--- lib/main.dart
+++ lib/main.dart
@@ -1,94 +1,132 @@
 import 'package:flutter/material.dart';
 
 void main() {
-  runApp(MaterialApp(
-      debugShowCheckedModeBanner: false,
-      title: "Contador de Pessoas",
-      home: Home()
-    )
-  );
+  runApp(MaterialApp(home: Home()));
 }
+
 class Home extends StatefulWidget {
   @override
   _HomeState createState() => _HomeState();
 }
 
 class _HomeState extends State<Home> {
+  TextEditingController alturaController = TextEditingController();
+  TextEditingController pesoController = TextEditingController();
 
-  int _contador = 0;
+  GlobalKey<FormState> _formKey = GlobalKey<FormState>();
 
-  String _msg = "Pode Entrar!";
+  String _msg = "Informe seus dados!";
 
-  void _contadorPessoa(int valor){
+  void _resetFild() {
+      alturaController.text = "";
+      pesoController.text = "";
     setState(() {
-      _contador += valor;
+      _msg = "Informe seus dados!";
+      _formKey = GlobalKey<FormState>();
     });
+  }
 
-    if(_contador < 0){
-      _msg = "Mundo Invertido?!";
-    }else if(_contador <= 10){
-      _msg = "Pode Entrar!";
-    }else{
-      _msg = "Lotado!";
-    }
+  void _calculate() {
+    setState(() {
+      double altura = double.parse(alturaController.text) / 100;
+      double peso = double.parse(pesoController.text);
+      double result = peso / (altura * altura);
+
+      if (result < 18.6) {
+        _msg = "Abaixo do Peso (${result.toStringAsPrecision(3)}) ";
+      } else if (result >= 18.6 && result < 24.9) {
+        _msg = "Peso Ideal (${result.toStringAsPrecision(3)}) ";
+      } else if (result >= 24.9 && result < 29.9) {
+        _msg = "Levemente Acima do Peso (${result.toStringAsPrecision(3)}) ";
+      } else if (result >= 29.9 && result < 34.9) {
+        _msg = "Obesidade Grau I (${result.toStringAsPrecision(3)}) ";
+      } else if (result >= 34.9 && result < 39.9) {
+        _msg = "Obesidade Grau II (${result.toStringAsPrecision(3)}) ";
+      } else if (result >= 40.0) {
+        _msg = "Obesidade Grau III (${result.toStringAsPrecision(3)}) ";
+      }
+    });
   }
 
   @override
   Widget build(BuildContext context) {
-    return Stack(
-        children: <Widget>[
-          Image.asset(
-            "images/original.jpg",
-            fit: BoxFit.cover,
-            height: 1000.0,
-          ),
-          Column(
-            mainAxisAlignment: MainAxisAlignment.center,
+    return Scaffold(
+      appBar: AppBar(
+        title: Text("Calculadora de IMC"),
+        centerTitle: true,
+        backgroundColor: Colors.black,
+        actions: <Widget>[
+          IconButton(icon: Icon(Icons.refresh), onPressed: _resetFild)
+        ],
+      ),
+      backgroundColor: Colors.white,
+      body: SingleChildScrollView(
+        padding: EdgeInsets.fromLTRB(10.0, 0.0, 10.0, 0.0),
+        child: Form(
+          key: _formKey,
+          child: Column(
+            crossAxisAlignment: CrossAxisAlignment.stretch,
             children: <Widget>[
-              Text(
-                "Pessoas: $_contador",
-                style:
-                    TextStyle(color: Colors.white, fontWeight: FontWeight.bold),
+              Icon(
+                Icons.person_outline,
+                size: 120.0,
+                color: Colors.black,
               ),
-              Row(
-                mainAxisAlignment: MainAxisAlignment.center,
-                children: <Widget>[
-                  Padding(
-                    padding: EdgeInsets.all(10.0),
-                    child: FlatButton(
-                      onPressed: () {
-                        _contadorPessoa(1);
-                      },
-                      child: Text(
-                        "+1",
-                        style: TextStyle(color: Colors.white, fontSize: 40),
-                      ),
-                    ),
+              TextFormField(
+                keyboardType: TextInputType.number,
+                decoration: InputDecoration(
+                    labelText: "Peso (kg)",
+                    labelStyle: TextStyle(
+                      color: Colors.black,
+                    )),
+                textAlign: TextAlign.center,
+                style: TextStyle(color: Colors.black, fontSize: 25.0),
+                controller: pesoController,
+                validator: (value){
+                  if(value.isEmpty){
+                    return "Inserir seu Peso!";
+                  }
+                },
+              ),
+              TextFormField(
+                keyboardType: TextInputType.number,
+                decoration: InputDecoration(
+                    labelText: "Altura (cm)",
+                    labelStyle: TextStyle(color: Colors.black)),
+                textAlign: TextAlign.center,
+                style: TextStyle(color: Colors.black, fontSize: 25.0),
+                controller: alturaController,
+                validator: (value){
+                  if(value.isEmpty){
+                    return "Inserir sua Altura!";
+                  }
+                },
+              ),
+              Container(
+                padding: EdgeInsets.fromLTRB(0.0, 10.0, 0.0, 10.0),
+                height: 75.0,
+                child: RaisedButton(
+                  onPressed: (){
+                    if(_formKey.currentState.validate()){
+                      _calculate();
+                    }
+                  },
+                  child: Text(
+                    "Calcular",
+                    style: TextStyle(color: Colors.white, fontSize: 20.0),
                   ),
-                  Padding(
-                    padding: EdgeInsets.all(10.0),
-                    child: FlatButton(
-                      onPressed: () {
-                        _contadorPessoa(-1);
-                      },
-                      child: Text(
-                        "-1",
-                        style: TextStyle(color: Colors.white, fontSize: 40),
-                      ),
-                    ),
-                  )
-                ],
+                  color: Colors.black,
+                ),
               ),
               Text(
                 _msg,
-                style: TextStyle(
-                    color: Colors.white,
-                    fontStyle: FontStyle.italic,
-                    fontSize: 30),
-              )
+                textAlign: TextAlign.center,
+                style: TextStyle(color: Colors.black, fontSize: 20.0),
+              ),
             ],
-          )
-        ],
-      );
+          ),
+        ),
+      ),
+    );
   }
 }
